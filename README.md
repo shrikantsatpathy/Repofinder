eb275f310d366c7931c8852102016e77ffdc2abe
Shrikant Satpathy <shrikant@Shrikants-MacBook-Pro-3.local>
2020-11-13 12:45:10 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Repofinder
+
+Repofinder is a webapplication which can find top n repositories(based on the fork count) and their top m contributors(based on their commits count) of your favourite organization on github
+
+### How to set it up locally on your machine
+You should have nodejs and npm installed in your machine. [click here](http://daringfireball.net/projects/markdown/) if you haven't already.
+Before running the application you need to register the application and get the client_id and secret_key.
+
+Register your application :
+  - [click here](https://github.com/settings/applications/new)
+  - Enter the Application Name, e.g. "Repofinder"
+  - Enter Homepage URL as "http://localhost:8080/"
+  - Enter Authorization callback URL as "http://localhost:8080/github/callback"
+  - Save the Client ID and Client secret, it will be needed later
+  
+Steps :
+  - Clone this repository.
+  - Open the terminal in the cloned repository directory
+  - run ```npm install``` to install all the dependencies
+  - Create a ".env" file in the cloned directory and store the Client ID and Client secret inside it as:
+  ``` 
+    GITHUB_CLIENT_ID = "Your_Client_Id"
+    GITHUB_CLIENT_SECRET = "Your_Client_Secret"
+  ```
+  - run ```node index.js``` top start the server. The server will be listening on port 8080
+  - open browser and go to "http://localhost:8080/"
+
+
+
+### How to use it
+
+You need to first click the "Authenticate" button this will generate the access token and you will be redirected to the form. Then you need to fill the form and click on "Search Repos". After few seconds you will be redirected to the home page and the result will be showed. To search again click on "Authenticate" button.
+
+![Alt Text](RepofinderTest.gif)
+### Note:
+- Some api calls return error. For example :
+    To get the contributors of "https://github.com/google/capsicum-linux"
+  https://api.github.com/repos/google/capsicum-linux/contributors shows
+![apifail](apifail.png)  
+However this case is handled but it will not show the top contributors of that particular repository. For example.  
+![authorfail](authorfail.png)
+
+- For large value of N there will be longer waiting time because API is called for each repository to get the top contributors . Wait until you're redirected to the home page. It may take upto 3-4 minutes.
+-  The client-id and secret key are required to make an authenticated api request. This is done because for unauthenticated requests, the rate limit allows for up to 60 requests per hour but for API requests using Basic Authentication or OAuth, you can make up to 5000 requests per hour. For more details [click here](https://developer.github.com/v3/)
+-  I have taken the assumption that the input m i.e the number of top author for a particular repository cannot be more the 100.
+
+
+
---
+++ index.js
@@ -0,0 +1,209 @@
+//Constants
+var express = require('express')
+var app = express()
+var axios = require('axios');
+var bodyParser = require('body-parser')
+var port = process.env.PORT || 8080
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var access_token = ""
+var repoDetailsViewPath = "/views/repodetails"
+
+require('dotenv').config();
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use("/static", express.static('./static/'));
+app.use(express.static(__dirname + '/public'));
+
+// This is the callback method for github to generate the authentication token
+app.get('/github/callback', (req, res) => {
+    console.log("hit /github/callback")
+    const requestToken = req.query.code
+    const gitHubAuthUrl = `https://github.com/login/oauth/access_token?client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_CLIENT_SECRET}&code=${requestToken}`
+    axios({
+        method: 'post',
+        url: gitHubAuthUrl,
+        headers: {
+            accept: 'application/json'
+        }
+    }).then((response) => {
+        console.log("Sucesfully authenticated the user")
+        access_token = response.data.access_token
+        repoDetailsList = []
+        res.redirect("/repoDetailsform")
+    })
+})
+
+/**
+ * This will be executed when the user hits the root url.
+ */
+app.get('/repoDetailsform', (req, res) => {
+    repoDetailsList = []
+    res.render(__dirname + repoDetailsViewPath, { repoDetailsList: repoDetailsList })
+})
+app.get('/', (req, res) => {
+    console.log('Loading home page')
+    repoDetailsList = []
+    res.render(__dirname + "/views/main.ejs", { repoDetailsList: repoDetailsList })
+})
+
+
+/**
+ * This will be executed when the user submitted the input to get the top N authors of the top M repositories.
+ */
+app.post('/', urlencodedParser, function (req, res) {
+    try {
+        console.log("post")
+        renderRepoDetails(req, res);
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+/**
+ * This method is responsible to render the repository details in the UI.
+ * @param {*} req 
+ * @param {*} res 
+ */
+async function renderRepoDetails(req, res) {
+    var organization = req.body.orgname;
+    var noOfRepos = req.body.n;
+    var noOfAuthors = req.body.m;
+    var repoDetailsList = new Array();
+
+    var topRepoList = await getTopRepos(organization, noOfRepos);
+
+    for (let i = 0; i < topRepoList.length; i++) {
+        topAuthors = await getTopAuthors(topRepoList[i], noOfAuthors).catch(error => {
+            console.log(error);
+        });;
+        repoDetailsList.push(new RepoDetail(topRepoList[i].full_name, topRepoList[i].forks_count, topAuthors));
+    }
+    res.render(__dirname + "/views/main.ejs", { repoDetailsList: repoDetailsList });
+    console.log('Loaded the repository details')
+}
+
+/**
+ * Get top respositories.
+ * @param {*} organization organization name for which the method returns the top repositories
+ * @param {*} noOfRepos No of top repositories to be returned
+ */
+async function getTopRepos(organization, noOfRepos) {
+
+    // Used client_id and client_secret to increase API call limit
+    var searchOrgReposUrl = "https://api.github.com/orgs/" + organization + "/repos?per_page=100";
+    var topRepoList = [];
+    await axios.get(searchOrgReposUrl, {
+        headers: {
+            Authorization: 'token ' + access_token
+        }
+    }).then(async (resp) => {
+        const link = resp.headers.link;
+        var totalPages = link ? parseInt(link.split(",")[1].split(">")[0].split("&page=")[1]) : 1
+        console.log(totalPages + " totalPages")
+        if (totalPages > 1) { // Organization with more than 1 page 
+            topRepoList = await getTopReposFromMultiplePages(totalPages, organization, noOfRepos);
+        }
+        else { // Organization with only 1 page
+            console.log("Getting top repos for organization " + organization + " with single page")
+            topRepoList = resp.data.sort((a, b) => b.forks_count - a.forks_count).slice(0, noOfRepos);
+            console.log("Fetched top repos for organization " + organization + " with single page")
+        }
+    })
+        .catch(error => {
+            console.log(error);
+        });
+
+    return topRepoList;
+}
+
+/**
+ * This method is responsible to get the Top N repositories for org with more than one page of repositories.
+ * @param {Array} orgName The name of the organization for which the method returns top N repos
+ * @param {Intege} n The number of repositories to be returned by the method
+ */
+async function getTopReposFromMultiplePages(noOfPages, orgName, n) {
+
+    const repoUrlListPerPage = []
+    var flag = true;
+    var topNReposAcrossPages = []
+    console.log("Getting top repos for organization " + orgName + " with multiple page")
+
+    for (let i = 1; i < 2 + 1; i++) {
+        // if (topNReposAcrossPages.length < n) {
+        repoPageUrl = `https://api.github.com/search/repositories?q=org:${orgName}&sort=forks&order=desc&per_page=100&page=` + i
+        await axios.get(repoPageUrl, {
+            headers: {
+                Authorization: 'token ' + access_token
+            }
+        }).then(resp => {
+            //Sorting top n respository of particular page based on fork_count (total fork) and slicing top n           
+            resp.data.items.forEach(element => {
+                // if (topNReposAcrossPages.length < n) {
+                topNReposAcrossPages.push(element)
+                //  }
+            }
+            )
+        });
+        // } else {
+        //     break;
+        // }
+    }
+
+    console.log("Fetched top repos for organization " + orgName + " with multiple page")
+    return topNReposAcrossPages.slice(0, n);
+}
+
+/**
+ * Get the top m authors for the given repository
+ * @param {*} repo repository name
+ * @param {*} m  number of authors for the given  repo
+ */
+async function getTopAuthors(repo, m) {
+    getAuthorUrl = `https://api.github.com/repos/` + repo.full_name + `/contributors?per_page=100&page=1`;
+    //console.log("Getting top authors for repository : " + repo.full_name)
+    try {
+        var sortedAuthorsJsonList = await axios.get(getAuthorUrl, {
+            headers: {
+                Authorization: 'token ' + access_token
+            }
+        })
+        sortedAuthorsJsonList = sortedAuthorsJsonList.data.slice(0, m)
+        var sortedAuthorList = sortedAuthorsJsonList.map(value => {
+            return {
+                name: value.login,
+                contributions: value.contributions
+            }
+        })
+        //console.log("Fetched top authors for repository : " + repo.full_name)
+        return sortedAuthorList;
+    }
+    catch (error) {
+        //The history or contributor list is too large to list contributors for this repository via the API
+        console.log("Can't fetch results for url : " + getAuthorUrl);
+        return [{
+            name: "cannot get top Authors please visit github",
+            contributions: ""
+        }]
+    }
+
+}
+
+/**
+ * This class contains the details of the repository to be displayed in the UI
+ */
+class RepoDetail {
+    constructor(repoName, forkCount, authors) {
+        this.repoName = repoName
+        this.forkCount = forkCount
+        this.authors = authors
+    }
+}
+
+//Start the server
+var server = app.listen(port, function () {
+    console.log("app running" + port)
+})
+server.setTimeout(500000);
+
+
